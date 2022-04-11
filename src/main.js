c5bca3de07a453e4e2e923ae3ef048250748d85e
Paolo Rechia <paolorechia@gmail.com>
2022-04-11 12:27:42 +0200

    Fix dependency

--- src/main.js
+++ src/main.js
@@ -3,17 +3,15 @@ import { div, attach } from "./view"
 
 const ROOT_ID = "SUPER_ROOT"
 
-function init() {
-    store.dispatch({ type: 'counter/incremented' })
-    // {value: 1}
-    store.dispatch({ type: 'counter/incremented' })
-    // {value: 2}
-    store.dispatch({ type: 'counter/decremented' })
-    // {value: 1}
+function render(store) {
+    console.log(store.getState())
+}
 
+function init() {
     let myDiv = div()
     myDiv.classList.add(".test-class")
     attach(ROOT_ID, myDiv)
+    store.subscribe(() => render)
 }
 
 init()

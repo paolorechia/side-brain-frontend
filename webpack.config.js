e712156b4fa0a4681d87189b573803c4642554a6
Paolo Rechia <paolorechia@gmail.com>
2022-04-11 10:40:35 +0200

    Bootstrap Project

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'lib'),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
+
